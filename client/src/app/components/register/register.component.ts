e09088c3b4a717152412abf4972a50c6204b0f88
Roman <ayoz4@yandex.ru>
2022-11-23 21:36:20 -0500

    [blog-06] added register and login forms

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  constructor(
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: new FormControl(null, [Validators.required]),
+      username: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(6),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      passwordConfirmation: new FormControl(null, [Validators.required])
+    })
   }
 
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    console.log(this.registerForm.value);
+
+    this.authService.register(this.registerForm.value).pipe(
+      map(user => this.router.navigate(['login']))
+    ).subscribe();
+  }
 }
